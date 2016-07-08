7a4d3e82f5e04bfc4d582e52a65b13f2eaf1f9b8
jcoope8190 <joshua.cooper@wework.com>
2016-07-07 21:08:03 -0400

    Update two

--- js/app.js
+++ js/app.js
@@ -1,168 +1,179 @@
-var location = [
-  {name: 'Irving Place',
-  dogFriendly: true,
-  communityManagers: true,
-  conferenceRooms: true,
-  eventSpace: true,
-  highSpeedInternet: true,
-  bikeStorage: true,
-  lounges: true,
-  microRoastedCoffee: true,
-  monthlyDemoDays: true,
-  printing: true,
-  privatePhoneBooths: true,
-  purifiedWater: true,
-  screeningRoom: true,
-  weeklyEvents: true,
-  uniqueInteriorDesign: true,
-  curatedEventsLineup: true,
-  freeBeer: true,
-  arcadeRoom: true,
-  bocceCourt: true,
-  helpfulBooks: true,
-  honestyMarket: true,
+$(function(){
+
+
+
+  $("#amenities").submit(function(event){
+    event.preventDefault();
+    console.log("Form submit");
+    var checkedElements = $("input[name='amenity']:checked");
+
+    var checkedValues = $.map(checkedElements, function(element){
+      console.log(element);
+      return element.value;
+    })
+
+    console.log(checkedValues);
+
+    // filterLocations(checkedValues)''
+
+  })
+
+  function filterLocations(checkedValues) {
+
+    _.filter(locations, ['dogFriendly', false]);
+
+    //1) Turn this 
+  //["communityManagers", "conferenceRooms"]
+
+
+  //2) Into this
+  // var query = {
+  //   "communityManagers": true,
+  //   "conferenceRooms": true,
+  // }
+
+
+  // filter using lodash
+   var foundLocations = _.filter(locatons, query);
+   console.log()
+
+   return foundLocations
 
   }
 
-  {name: 'Times Square',
-  dogFriendly: true,
-  communityManagers: true,
-  conferenceRooms: true,
-  eventSpace: true,
-  highSpeedInternet: true,
-  bikeStorage: true,
-  lounges: true,
-  microRoastedCoffee: true,
-  monthlyDemoDays: true,
-  printing: true,
-  privatePhoneBooths: true,
-  purifiedWater: true,
-  screeningRoom: true,
-  weeklyEvents: true,
-  uniqueInteriorDesign: true,
-  curatedEventsLineup: true,
-  freeBeer: true,
-  arcadeRoom: true,
-  bocceCourt: true,
-  helpfulBooks: true,
-  honestyMarket: true,
-  }
 
-  {name: 'Soho West',
-  dogFriendly: true,
-  communityManagers: true,
-  conferenceRooms: true,
-  eventSpace: true,
-  highSpeedInternet: true,
-  bikeStorage: true,
-  lounges: true,
-  microRoastedCoffee: true,
-  monthlyDemoDays: true,
-  printing: true,
-  privatePhoneBooths: true,
-  purifiedWater: true,
-  screeningRoom: true,
-  weeklyEvents: true,
-  uniqueInteriorDesign: true,
-  curatedEventsLineup: true,
-  freeBeer: true,
-  arcadeRoom: false,
-  bocceCourt: false,
-  helpfulBooks: false,
-  honestyMarket: false,
 
-  }
 
-  {name: 'Charging Bull',
-  dogFriendly: true,
-  communityManagers: true,
-  conferenceRooms: true,
-  eventSpace: true,
-  highSpeedInternet: true,
-  bikeStorage: true,
-  lounges: true,
-  microRoastedCoffee: true,
-  monthlyDemoDays: true,
-  printing: true,
-  privatePhoneBooths: true,
-  purifiedWater: true,
-  screeningRoom: true,
-  weeklyEvents: true,
-  uniqueInteriorDesign: true,
-  curatedEventsLineup: true,
-  freeBeer: true,
-  arcadeRoom: true,
-  bocceCourt: true,
-  helpfulBooks: true,
-  honestyMarket: true,
-  }
 
-  {name: 'Park South',
-  dogFriendly: true,
-  communityManagers: true,
-  conferenceRooms: true,
-  eventSpace: true,
-  highSpeedInternet: true,
-  bikeStorage: true,
-  lounges: true,
-  microRoastedCoffee: true,
-  monthlyDemoDays: true,
-  printing: true,
-  privatePhoneBooths: true,
-  purifiedWater: true,
-  screeningRoom: true,
-  weeklyEvents: true,
-  uniqueInteriorDesign: true,
-  curatedEventsLineup: true,
-  freeBeer: true,
-  arcadeRoom: true,
-  bocceCourt: true,
-  helpfulBooks: true,
-  honestyMarket: true,
-  }
+
+
+
+
+  var locations = [
+  {
+    name: 'Irving Place',
+    dogFriendly: false,
+    communityManagers: true,
+    conferenceRooms: true,
+    // eventSpace: false,
+    // highSpeedInternet: true,
+    // bikeStorage: false,
+    // lounges: true,
+    // microRoastedCoffee: true,
+    // monthlyDemoDays: false,
+    // printing: true,
+    // privatePhoneBooths: false,
+    // purifiedWater: true,
+    // screeningRoom: false,
+    // weeklyEvents: true,
+    // uniqueInteriorDesign: true,
+    // curatedEventsLineup: false,
+    // freeBeer: true,
+    // arcadeRoom: false,
+    // bocceCourt: false,
+    // helpfulBooks: false,
+    // honestyMarket: false,
+  },
+
+   {
+    name: 'Times Square',
+    dogFriendly: false,
+    communityManagers: true,
+    conferenceRooms: true,
+    // eventSpace: true,
+    // highSpeedInternet: true,
+    // bikeStorage: false,
+    // lounges: true,
+    // microRoastedCoffee: true,
+    // monthlyDemoDays: false,
+    // printing: true,
+    // privatePhoneBooths: false,
+    // purifiedWater: true,
+    // screeningRoom: false,
+    // weeklyEvents: true,
+    // uniqueInteriorDesign: true,
+    // curatedEventsLineup: false,
+    // freeBeer: true,
+    // arcadeRoom: false,
+    // bocceCourt: false,
+    // helpfulBooks: false,
+    // honestyMarket: false,
+  },
+
+  {
+    name: 'Soho West',
+    dogFriendly: true,
+    communityManagers: true,
+    conferenceRooms: true,
+    // eventSpace: true,
+    // highSpeedInternet: true,
+    // bikeStorage: true,
+    // lounges: true,
+    // microRoastedCoffee: true,
+    // monthlyDemoDays: true,
+    // printing: true,
+    // privatePhoneBooths: true,
+    // purifiedWater: true,
+    // screeningRoom: true,
+    // weeklyEvents: true,
+    // uniqueInteriorDesign: true,
+    // curatedEventsLineup: true,
+    // freeBeer: true,
+    // arcadeRoom: false,
+    // bocceCourt: false,
+    // helpfulBooks: false,
+    // honestyMarket: false,
+  },
+
+  // {
+  //   name: 'Charging Bull',
+  //   dogFriendly: false,
+  //   communityManagers: true,
+  //   conferenceRooms: true,
+  //   eventSpace: true,
+  //   highSpeedInternet: true,
+  //   bikeStorage: true,
+  //   lounges: true,
+  //   microRoastedCoffee: true,
+  //   monthlyDemoDays: false,
+  //   printing: true,
+  //   privatePhoneBooths: true,
+  //   purifiedWater: true,
+  //   screeningRoom: false,
+  //   weeklyEvents: true,
+  //   uniqueInteriorDesign: true,
+  //   curatedEventsLineup: true,
+  //   freeBeer: true,
+  //   arcadeRoom: true,
+  //   bocceCourt: true,
+  //   helpfulBooks: true,
+  //   honestyMarket: true,
+  // },
+
+  // {
+  //   name: 'Park South',
+  //   dogFriendly: false,
+  //   communityManagers: true,
+  //   conferenceRooms: true,
+  //   eventSpace: true,
+  //   highSpeedInternet: true,
+  //   bikeStorage: false,
+  //   lounges: true,
+  //   microRoastedCoffee: true,
+  //   monthlyDemoDays: false,
+  //   printing: true,
+  //   privatePhoneBooths: true,
+  //   purifiedWater: true,
+  //   screeningRoom: false,
+  //   weeklyEvents: true,
+  //   uniqueInteriorDesign: true,
+  //   curatedEventsLineup: true,
+  //   freeBeer: true,
+  //   arcadeRoom: true,
+  //   bocceCourt: false,
+  //   helpfulBooks: false,
+  //   honestyMarket: true,
+  // }
 ]
 
-
-
-
-
-$(function(){
-  // Check
-$("#checkbox").attr("checked", true);
-
-//  Uncheck
-$("#checkbox").attr("checked", false);
-
-var form = document.getElementById('amenity');
-
-function submitFn(event) {
-    event.preventDefault();
-    var boxes = document.getElementsByClassName('checkbox');
-    var checked = [];
-    for(var i=0; boxes[i]; ++i){
-      if(boxes[i].checked){
-        checked.push(boxes[i].name);
-      }
-    }
-
-    var checkedStr = checked.join();
-
-    document.getElementById('checkbox_str').value = checkedStr;
-    form.submit();
-
-    return false;
-}
-
-
-var checked = $('.names:checked').map(function() {
-  return this.value;
-}).get();
-if (checked.length) {
-  console.log(checked);
-} else {
-  console.log('null');
-}
-
-
-
-  }); 
+}); 
